05b8d900b4fc1745fe29b12aa8ea7b026eeeece9
Randhir Kumar Singh <97341921+heyrandhir@users.noreply.github.com>
2024-05-15 16:39:34 +0530

    prob 445

---
+++ linked-list/problem-445.java
@@ -0,0 +1,66 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return reverseLL(addLL(reverseLL(l1),reverseLL(l2)));
+    }
+
+    public ListNode reverseLL(ListNode l1) {
+        ListNode temp;
+        ListNode prev = null;
+        while (l1!= null) {
+            temp = l1.next;
+            l1.next = prev;
+            prev = l1;
+            l1 = temp;
+        }
+        return prev;
+    }
+
+    public ListNode addLL(ListNode l1, ListNode l2) {
+        
+        ListNode dummyNode = new ListNode(-1);
+        ListNode res = dummyNode;
+        int carry = 0;
+
+        while (l1 != null && l2 != null) {
+            ListNode sum = new ListNode((l1.val+l2.val+carry)%10);
+            carry = (l1.val+l2.val+carry)/10;
+            res.next = sum;
+            res = res.next;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        while (l1 != null) {
+            ListNode sum = new ListNode((l1.val+carry)%10);
+            carry = (l1.val+carry)/10;
+            res.next = sum;
+            res = res.next;
+            l1 = l1.next;
+        }
+
+        while (l2 != null) {
+            ListNode sum = new ListNode((l2.val+carry)%10);
+            carry = (l2.val+carry)/10;
+            res.next = sum;
+            res = res.next;
+            l2 = l2.next;
+        }
+
+        if (carry != 0) {
+            ListNode sum = new ListNode(carry);
+            res.next = sum;
+        }
+
+        return dummyNode.next;
+    }
+}
