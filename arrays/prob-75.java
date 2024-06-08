0751e4b1c91ecb1b45ea05e86dccb9008e71a81d
Randhir Kumar Singh <97341921+heyrandhir@users.noreply.github.com>
2024-06-08 14:58:13 +0530

    optimised solution in O ( N )

--- arrays/prob-75.java
+++ arrays/prob-75.java
@@ -1,23 +1,41 @@
 class Solution {
     public void sortColors(int[] nums) {
         
-        HashMap<Integer,Integer> memo = new HashMap<>();
-        for (int num: nums) {
-            if (!memo.containsKey(num)) {
-                memo.put(num,1);
+        // bruteforce approach 
+
+        // HashMap<Integer,Integer> memo = new HashMap<>();
+        // for (int num: nums) {
+        //     if (!memo.containsKey(num)) {
+        //         memo.put(num,1);
+        //     } else {
+        //         memo.put(num,memo.get(num)+1);
+        //     }
+        // }
+
+
+        // int count = 0;
+        // for (int color = 0; color <3; color++) {
+        //     if (memo.containsKey(color)) {
+        //         for (int i=0;i<memo.get(color);i++) {
+        //             nums[count+i] = color;
+        //         }
+        //         count+=memo.get(color);
+        //     }
+        // }
+
+        // optimised solution 
+        while (s <= e && i <= e) {
+            if (nums[i] == 0) {
+                nums[i] = nums[s];
+                nums[s] = 0;
+                i++;
+                s++; 
+            } else if (nums[i] == 2) {
+                nums[i] = nums[e];
+                nums[e] = 2;
+                e--;
             } else {
-                memo.put(num,memo.get(num)+1);
-            }
-        }
-
-
-        int count = 0;
-        for (int color = 0; color <3; color++) {
-            if (memo.containsKey(color)) {
-                for (int i=0;i<memo.get(color);i++) {
-                    nums[count+i] = color;
-                }
-                count+=memo.get(color);
+                i++;
             }
         }
     }
